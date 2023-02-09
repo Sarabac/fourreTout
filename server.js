5876d784ae88b6bd273707accd0f1a99c609b89c
bmathieugit <bmathieugit@gmail.com>
2023-02-09 10:03:20 +0100

    add timer to shutdown server.js (mouaaahahahaha)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = {{ SERVER_PORT }};
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,16 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
+const timeout = Math.floor(Math.random() * 60_000);
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`${new Date().toUTCString()} : Server running at http://${hostname}:${port}/ during ${timeout} ms`);
 });
+
+
+setTimeout(() => {
+  console.log(`${new Date().toUTCString()} : Server closing...`);
+  server.close();
+  console.log(`${new Date().toUTCString()} : Server closed`);
+}, timeout);
+
