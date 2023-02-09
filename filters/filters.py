fa8e46e3853c821e39e1f1f8fc56e53d8ea2f899
AKH-Soulay <soulaydu69@outlook.fr>
2023-02-09 10:03:28 +0100

    Last push

--- filters/filters.py
+++ filters/filters.py
@@ -1,14 +1,29 @@
 def add_suffix(word):
-    # ajoute l'extension .suffix au string en entrée
+    # ajoute un suffix au string en entrée
     word = str(word)
-    return word + ".suffix"
+    return word + ".Stupefix"
+
+def add_prefix(word):
+    # ajoute un prefix au string en entrée
+    word = str(word)
+    return "Prefafix-" + word
+
+def no_space(word):
+    if " " in word:
+        new_word = word.replace(" ", "-")
+        return new_word
+    else:
+        return word
+
 
 class FilterModule(object):
     ''' Ansible core jinja2 filters '''
 
     def filters(self):
         return {
-            'add_suffix': add_suffix
+            'add_suffix': add_suffix,
+            'add_prefix': add_prefix,
+            'no_space': no_space
         }
 
 # test = "test"
