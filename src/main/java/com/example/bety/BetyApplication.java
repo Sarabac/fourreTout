fb23dbee910604ba94067e883935e9486a988bf9
Sarabac <lucas.boutar@gmail.com>
2023-02-13 15:37:21 +0100

    premier commit java

---
+++ src/main/java/com/example/bety/BetyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetyApplication.class, args);
+	}
+
+}
