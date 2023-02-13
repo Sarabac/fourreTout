029ca0018051eb65e14df476aff987f3532d3c31
AKH-Soulay <soulaydu69@outlook.fr>
2023-02-13 16:03:49 +0100

    Dernier push - python ecris dans un texte

--- library/server_gestion.py
+++ library/server_gestion.py
@@ -1,24 +1,29 @@
-import os, time, subprocess, socket
+import time, subprocess, socket
+from mysql_creation import *
+from datetime import datetime
+
 
 # ==============================
 # fonctions
 
-def check_server(hostname: str) -> bool:
-    response = os.system("ping -c 1 " + hostname)
-    return response == 0
-    
 def start_server(path: str) -> subprocess.Popen:
     return subprocess.Popen(["node", path])
 
 def loop_check(path: str, host: str, SERVER_PORT: str):
     i = 0
+    # init_schema(Params)
     while i < 120:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((host,SERVER_PORT))
         if result == 0:
-            print("Success port open :)")
+            # print("Success port open :)")
+            pass
         else:
-            print("Oups, port not open !")
+            f = open("./library/sauv.txt", "a")
+            f.writelines([f"Oups, port not opened ! Server restart... ({datetime.now()})", "\n"])
+            # print("Oups, port not open ! Server restart")
+            # append_error(Params)
+            f.close()
             start_server(path)
         sock.close()
 
@@ -26,14 +31,16 @@ def loop_check(path: str, host: str, SERVER_PORT: str):
         i += 1
 
 # ==============================
-
-
-
-# ==============================
 # main
 
 SERVER_PORT = 3001
 path = "./template/server1.js"
 host = "localhost"
+Params = ConnectParam(
+    database="error_db",
+    host="localhost",
+    password="password",
+    user="root"
+)
 
 loop_check(path, host, SERVER_PORT)
