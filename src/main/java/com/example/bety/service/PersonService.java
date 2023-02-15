d4b7a7be81d9d580fd59cbbbbd330bd26b6a179a
YlliesSO <mouissat.yllies@hotmail.fr>
2023-02-15 16:48:46 +0100

    implem PersonService

--- src/main/java/com/example/bety/service/PersonService.java
+++ src/main/java/com/example/bety/service/PersonService.java
@@ -1,25 +1,45 @@
 package com.example.bety.service;
 
+import com.example.bety.model.bdd.PersonModel;
+import com.example.bety.model.mapper.PersonMapper;
 import com.example.bety.model.service.Person;
+import com.example.bety.repository.PersonRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PersonService {
 
-    public Person getPerson(Long id) {
-        return null;
+    PersonRepo personRepo;
+    PersonMapper personMapper;
+    PersonService(PersonRepo personRepo, PersonMapper personMapper) {
+        this.personRepo = personRepo;
+        this.personMapper = personMapper;
     }
 
-    public boolean addPerson(Person p) {
-        return false;
+    public Optional<Person> getPerson(Long id) {
+        Optional<PersonModel> oPersonModel = personRepo.findById(id);
+        return oPersonModel.map(personModel -> personMapper.bdd2Service(personModel));
     }
 
-    public boolean deletePerson(Person p) {
-        return false;
+    public Person addPerson(Person p) {
+        PersonModel personModel = personMapper.service2Bdd(p);
+        personModel.setId(null);
+        PersonModel resultat = personRepo.save(personModel);
+        return personMapper.bdd2Service(resultat);
+    }
+
+    public boolean deletePerson(Long id) {
+        boolean reponse = personRepo.existsById(id);
+        personRepo.deleteById(id);
+        return reponse;
     }
 
     public Person updatePerson(Person p) {
-        return p;
+        PersonModel personModel = personMapper.service2Bdd(p);
+        PersonModel resultat = personRepo.save(personModel);
+        return personMapper.bdd2Service(resultat);
     }
 
 }
