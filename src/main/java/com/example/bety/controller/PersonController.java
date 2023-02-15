94a47b1d61cd5c9b8c5213ae026a0f03586ae64f
YlliesSO <mouissat.yllies@hotmail.fr>
2023-02-15 17:53:13 +0100

    implem controller

---
+++ src/main/java/com/example/bety/controller/PersonController.java
@@ -0,0 +1,10 @@
+package com.example.bety.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping
+}
