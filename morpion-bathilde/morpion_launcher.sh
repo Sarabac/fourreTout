afbdecacdf3e2608c16bcc07fcaba600f6c799b2
bathou <bathou@unjoursamarchera.myguest.virtualbox.org>
2023-03-02 12:47:05 +0100

    tout fonctionne manque plus que le coté front

--- morpion-bathilde/morpion_launcher.sh
+++ morpion-bathilde/morpion_launcher.sh
@@ -1,1 +1,2 @@
-echo "le jeu est en cours de preparation"
+
+nohup java -jar /home/local/{{ dossier_server_morpion }}/{{ back_morpion_dest }} --spring.config.location={{ dossier_server_morpion }}/application.yaml &
